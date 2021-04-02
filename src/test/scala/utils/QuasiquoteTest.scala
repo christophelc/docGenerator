2fa6d36161119f56dc08c6fdb93f8e931677076f
Christophe Le Cam <clecam@talend.com>
2021-04-02 14:42:32 +0200

    Add tests

--- src/test/scala/utils/QuasiquoteTest.scala
+++ src/test/scala/utils/QuasiquoteTest.scala
@@ -37,18 +37,71 @@ class QuasiquoteTest extends Specification with MyLogger {
       parsedCode must not contain "error"
     }
 
-    "xxx" in {
+    "be parsed and work with pattern matching" in {
       val result = "function(arg1, arg2)".parse[Term].get match {
         case Term.Apply(function, List(arg1, arg2)) =>
           Seq(function, arg1, arg2)
       }
-      logger.info(s"""
-                     ***************
-                     ${result.mkString(",")}
-                     ***************
-        """)
       result.mkString("\n") must not contain "error"
     }
-
   }
+  "Traversal" should {
+    "show all nodes" in {
+      val s = q"val x = 2".collect {
+        case node => node.productPrefix -> node.toString
+      }
+//      logger.info(s"""
+//                     ***************
+//                     $s
+//                     ***************
+//        """)
+      s must not be empty
+    }
+  }
+
+  "Custon traversal" should {
+    "show details of the parsed nodes" in {
+      var mutParsed = List.newBuilder[String]
+      val traverser = new Traverser {
+        override def apply(tree: Tree): Unit = {
+
+          tree match {
+            case Pat.Var(name) =>
+              mutParsed += s"var: $name (stopped here)"
+            case node =>
+              mutParsed += s"${node.productPrefix} : $node"
+              super.apply(node)
+          }
+        }
+      }
+      traverser(q"val x = 2")
+      val s = mutParsed.result()
+//      logger.info(s"""
+//                     ***************
+//                     ${s.mkString(",")}
+//                     ***************
+//        """)
+      s.mkString(",") must not be empty
+    }
+  }
+
+  "Custom transform tree" should {
+    "transform 'a + b' to 'a + function(b)" in {
+      // be careful to infinite recursion; see https://scalameta.org/docs/trees/guide.html
+      val transformer = new Transformer {
+        override def apply(tree: Tree): Tree = tree match {
+          case name @ Term.Name("b") => q"function($name)"
+          case node                  => super.apply(node)
+        }
+      }
+      val newTree = transformer(q"a + b")
+      logger.info(s"""
+                           ***************
+                           ${newTree.toString()}
+                           ***************
+              """)
+      newTree.toString() must contain("function(b)")
+    }
+  }
+
 }
